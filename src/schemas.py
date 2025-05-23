0fa38d50214f338921375dbc80c120b854c48243
Jasper Zuyderwijk <zuyderwijk@gmail.com>
2025-05-24 00:13:01 +0200

    Adjust schema and handler

--- src/schemas.py
+++ src/schemas.py
@@ -6,22 +6,8 @@ Contains the schemas for input validation.
 
 # Input schema
 INPUT_SCHEMA = {
-    "data_url": {
-        "type": str,
-        "required": False
-    },
-    "data_urls": {
-        "type": list,
-        "required": False
-    },
-    "min_length": {
-        "type": int,
-        "required": False,
-        "default": 5
-    },
-    "max_length": {
-        "type": int,
-        "required": False,
-        "default": 75
-    }
+    "data_url": {"type": str, "required": False},
+    "data_urls": {"type": list, "required": False},
+    "min_length": {"type": int, "required": False, "default": 5},
+    "max_length": {"type": int, "required": False, "default": 75}
 }
